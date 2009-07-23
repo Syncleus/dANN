e1a739dd6b06a5eb8a23f9b9fa27d6759188310d
Jeffrey Phillips Freeman <jeffrey.freeman@syncleus.com>
2009-07-23 12:00:10 +0000

    moved the trunk distribution script to the java_dann-1.x branch
    git-svn-id: svn://svn.syncleus.com/dANN/branches/java_dann-1.x@328 6ae8b97b-f314-0410-8212-aecf10b92ded

--- dist/linux-dist.sh
+++ dist/linux-dist.sh
@@ -1,47 +1,113 @@
 #!/bin/sh
 
-#check to make sure it has an argument
-if [ $# -lt 1 ]
-then
-	echo "must specify the directory in tags to package for distribution."
+if hash svn2cl 2> /dev/null; then
+	echo "svn2cl found, good..."
+else
+	echo "svn2cl must be on your path"
 	exit 1
 fi
 
+if hash svn 2> /dev/null; then
+	echo "svn found, good..."
+else
+	echo "svn must be on your path"
+	exit 1
+fi
+
+if hash tar 2> /dev/null; then
+	echo "tar found, good..."
+else
+	echo "tar must be on your path"
+	exit 1
+fi
+
+if hash ant 2> /dev/null; then
+	echo "ant found, good..."
+else
+	echo "ant must be on your path"
+	exit 1
+fi
+
+if hash rm 2> /dev/null; then
+	echo "rm found, good..."
+else
+	echo "rm must be on your path"
+	exit 1
+fi
+
+
+#pull the arguments
+if [ $# -lt 3 ]
+then
+	ARCH_NAME="java_dann-trunk"
+	SVN_ROOT="trunk/projects"
+else
+	if [ $# -lt 4 ]
+	then
+		ARCH_NAME=$3
+		SVN_ROOT="tags/$ARCH_NAME"
+	else
+		ARCH_NAME=$4
+		SVN_ROOT="$3/$ARCH_NAME"
+	fi
+fi
+
+if [ $# -lt 2 ]
+then
+	REV=HEAD
+else
+	REV=$2
+fi
+
+if [ $# -lt 1 ]
+then
+	echo "usage: $0 <output dir> [revision] [tags|branches] [tag/branch name]"
+	exit 1
+else
+	OUT_DIR=$1
+fi
+
+SVN_URL="svn://svn.syncleus.com/dANN"
+
+
 #check out tag
-svn co svn://svn.syncleus.com/dANN/tags/$1 ./tmp/$1-co
-svn export svn://svn.syncleus.com/dANN/tags/$1 ./tmp/$1
+svn co -r $REV $SVN_URL/$SVN_ROOT ./tmp/$ARCH_NAME-co
+svn export -r $REV $SVN_URL/$SVN_ROOT ./tmp/$ARCH_NAME
 
 #Generate ChangeLog
-svn2cl -i --group-by-day --authors authors.xml -o ./tmp/$1/java_dann/doc/ChangeLog ./tmp/$1-co/java_dann
-svn2cl -i --group-by-day --authors authors.xml -o ./tmp/$1/java_dann_examples/doc/ChangeLog ./tmp/$1-co/java_dann_examples
+svn2cl -i --group-by-day --authors authors.xml -o ./tmp/$ARCH_NAME/java_dann/doc/ChangeLog ./tmp/$ARCH_NAME-co/java_dann
+svn2cl -i --group-by-day --authors authors.xml -o ./tmp/$ARCH_NAME/java_dann_examples/doc/ChangeLog ./tmp/$ARCH_NAME-co/java_dann_examples
 
 # tarball source distribution
-tar -czvf $1-src.tar.gz -C ./tmp/ $1
+tar -czvf $OUT_DIR/$ARCH_NAME-src.tar.gz -C ./tmp/ $ARCH_NAME
 
 #remove dist dir for binary distribution
-rm -rf ./tmp/$1/dist
+rm -rf ./tmp/$ARCH_NAME/dist
 
 #compile core library
-ant -buildfile ./tmp/$1/java_dann/build.xml build-all javadoc
+ant -buildfile ./tmp/$ARCH_NAME/java_dann/build.xml build-all javadoc
 
 #clean core library to include only files for binary distribution
-rm -rf ./tmp/$1/java_dann/src
-rm -rf ./tmp/$1/java_dann/build/classes
-rm -rf ./tmp/$1/java_dann/build/coverage
-rm -rf ./tmp/$1/java_dann/build/tests
-rm -f ./tmp/$1/java_dann/cobertura.ser
+rm -rf ./tmp/$ARCH_NAME/java_dann/src
+rm -rf ./tmp/$ARCH_NAME/java_dann/build/classes
+rm -rf ./tmp/$ARCH_NAME/java_dann/build/coverage
+rm -rf ./tmp/$ARCH_NAME/java_dann/build/tests
+rm -f ./tmp/$ARCH_NAME/java_dann/cobertura.ser
 
 #compile examples application
-ant -buildfile ./tmp/$1/java_dann_examples/build.xml all
+ant -buildfile ./tmp/$ARCH_NAME/java_dann_examples/build.xml all
 
 #clean examples application to include only files for binary distribution
-rm -rf ./tmp/%1/java_dann_examples/src
-rm -rf ./tmp/%1/java_dann_examples/build/classes
+rm -rf ./tmp/$ARCH_NAME/java_dann_examples/src
+rm -rf ./tmp/$ARCH_NAME/java_dann_examples/build/classes
 
 # tarball binary distribution
-tar -czvf $1-bin.tar.gz -C ./tmp/ $1
+tar -czvf $OUT_DIR/$ARCH_NAME-bin.tar.gz -C ./tmp/ $ARCH_NAME
+
+# tarball javadocs
+tar -czvf $OUT_DIR/$ARCH_NAME-javadoc.tar.gz -C ./tmp/$ARCH_NAME/java_dann/build/ javadoc/
 
 #remove tmp directory
 rm -rf ./tmp
 
-exit 0
+#exit 0
